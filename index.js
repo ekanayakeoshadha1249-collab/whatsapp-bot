410c9c39cc7639d5aecc9f612c87523b244c2685
ekanayakeoshadha1249-collab <ekanayakeoshadha1249@gmail.com>
2026-02-14 20:59:41 +0530

    Initialize WhatsApp bot with QR code authentication

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+  console.log('Scan QR code');
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('WhatsApp Bot is READY!');
+});
+
+client.on('message', msg => {
+  if (msg.body === 'hi') {
+    msg.reply('Hello 👋 I am online WhatsApp bot!');
+  }
+});
+
+client.initialize();
